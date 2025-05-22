c79b50b5175fd84da79c8997b6552c4436e2e0cb
VireshReddy123 <viresh.reddy@sqelabs.com>
2025-05-22 18:11:08 +0530

    Different types of variables

---
+++ variable.js
@@ -0,0 +1,61 @@
+// Variables in javascript
+
+
+// Var Type Variable 
+
+// Observations :
+  // 1. Var variable is use to global declaration.
+
+
+var a= 16;
+var b="Name"
+var c= 25;
+console.log(a)
+console.log(c)
+
+{
+    var c="My name is viresh"
+    console.log(c)
+}
+    console.log(c)
+
+
+    
+// let Type Variable 
+
+// Observations :
+
+// let variable is used for local declaration.
+
+let d= 16;
+let e="Name"
+let f= 25;
+console.log(d)
+console.log(e)
+
+{
+    let f="My name is viresh"
+    console.log(f)
+}
+    console.log(f)
+
+
+
+// const Type Variable 
+
+// Observations :
+
+// const variable is neither changed and not updated.
+// 
+
+const g= 16;
+g=20;
+console.log(g) 
+
+
+{
+    const g="My name is viresh"
+    console.log(g)
+}
+    console.log(g)
+
